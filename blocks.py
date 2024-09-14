a13820e1133fa122e9f70474df354f4800ac6f7f
leesolivia14 <sl8055@nyu.edu>
2024-09-13 22:13:26 -0700

    block inheritance!

---
+++ blocks.py
@@ -0,0 +1,12 @@
+from block import Block
+from position import Position
+
+class LBlock(Block):
+    def __init__(self):
+        super().__init__(id = 1)
+        self.cells = {
+            0: [Position(0,2), Position(1,0), Position(1,1), Position(1,2)], #the coordinates filled if you think of each block as a 3x3 grid
+            1: [Position(0,1), Position(1,1), Position(2,1), Position(2,2)],
+            2: [Position(1,0), Position(1,1), Position(1,2), Position(2,0)],
+            3: [Position(0,0), Position(0,1), Position(1,1), Position(2,1)]
+        }
