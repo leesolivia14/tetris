0b3ae2cff717ee28d37acd4ff5f9106a3c602dbf
leesolivia14 <sl8055@nyu.edu>
2024-11-17 21:38:09 -0800

    move blocks based on user key input

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pygame, sys
-from grid import Grid
-from blocks import *
+from game import Game
 
 pygame.init()
 dark_blue = (44, 44, 127)
@@ -11,15 +10,7 @@ pygame.display.set_caption("Python Tetris")
 
 clock = pygame.time.Clock()
 
-
-game_grid = Grid()
-
-block = OBlock()
-# game_grid.grid[0][0] = 1
-# game_grid.grid[3][5] = 4
-# game_grid.grid[17][8] = 7
-
-# game_grid.print_grid()
+game = Game()
 
 # get all the events that pygame recognizes
 while True:
@@ -27,10 +18,18 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                game.move_left()
+            if event.key == pygame.K_RIGHT:
+                game.move_right()
+            if event.key == pygame.K_UP:
+                game.move_up()
+            if event.key == pygame.K_DOWN:
+                game.move_down()            
     # Drawing
     screen.fill(dark_blue)
-    game_grid.draw(screen)
-    block.draw(screen)
+    game.draw(screen)
 
     pygame.display.update()
     clock.tick(60)
