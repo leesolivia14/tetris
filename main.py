f3d90bc393afdbb1b3c541b5584c730bc65ec748
leesolivia14 <sl8055@nyu.edu>
2024-11-18 18:28:15 -0800

    adding score, game over, next block ui

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import pygame, sys
 from game import Game
+from colors import Colors
 
 pygame.init()
 
 title_font = pygame.font.Font(None, 40)
+score_surface = title_font.render("Score", True, Colors.white)
+next_surface = title_font.render("Next", True, Colors.white)
+game_over_surface = title_font.render("GAME OVER", True, Colors.white)
 
-dark_blue = (44, 44, 127)
+score_rect = pygame.Rect(320, 55, 170, 60)
+next_rect = pygame.Rect(320, 215, 170, 180)
 
 # creates a display surface object called screen. the game window!
 screen = pygame.display.set_mode((500, 620))
@@ -39,7 +44,14 @@ while True: # game loop is 60 fps and it loops 60 times per second
         if event.type == GAME_UPDATE and game.game_over == False:
             game.move_down() # update block position when it has to not just when the loop executes
     # Drawing
-    screen.fill(dark_blue)
+    screen.fill(Colors.dark_blue)
+    screen.blit(score_surface, (365, 20, 50, 50))
+    screen.blit(next_surface,(375, 180, 50, 50))
+    if game.game_over == True:
+        screen.blit(game_over_surface,(320, 450, 50, 50))
+
+    pygame.draw.rect(screen, Colors.light_blue, score_rect, 0, 10)
+    pygame.draw.rect(screen, Colors.light_blue, next_rect, 0, 10)
     game.draw(screen)
 
     pygame.display.update()
